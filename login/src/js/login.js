e668730336e6cfe2f9211631feddedf841c5743d
aasd755326 <38196667+aasd755326@users.noreply.github.com>
2020-05-21 02:51:26 +0800

    Update login.js

--- login/src/js/login.js
+++ login/src/js/login.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             const form = document.forms['form']
             const appellation = form.elements.appellation.value;
             const account = form.elements.account.value;
-            const password = form.elements.account.valuel
+            const password = form.elements.password.value;
             alert("appellation: "+appellation+"\naccount: "+account+"\npassword: "+password);
         });
     });
