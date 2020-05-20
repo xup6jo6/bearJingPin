840804d641103d403522126da9ab83999e9ae8c0
xup6jo6 <hkhs0407@gmail.com>
2020-05-21 07:24:50 +0800

    [add] add homePage

---
+++ Home/header.js
@@ -0,0 +1,17 @@
+header = '\
+	<img id="pooh" src="./mphoto/chinese.png">\
+	<img id="E0" src="./mphoto/E0.png">\
+	<img id="photo" src="./mphoto/photoicon.png">\
+	<img id="EXP" src="./mphoto/EXP.png">\
+	<img id="money" src="./mphoto/money.png">\
+	<img id="map" src="./mphoto/map.png">\
+	<img id="EXPi" src="./mphoto/EXPi.png">\
+	<img id="EXPo" src="./mphoto/EXPo.png">\
+	<img id="hungry" src="./mphoto/hungry.png">\
+	<img id="huni" src="./mphoto/huni.png">\
+	<img id="huno" src="./mphoto/huno.png">\
+	<img id="love" src="./mphoto/love2.png">\
+	<img id="loveb" src="./mphoto/loveb2.png">\
+	<img id="lv5" src="./mphoto/lv5.png">\
+	<img id="top" src="./mphoto/top.png">';
+document.write(header);
