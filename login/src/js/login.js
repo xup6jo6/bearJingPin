ee1f6a7ba0b9157b3038d10022436095e8532734
aasd755326 <38196667+aasd755326@users.noreply.github.com>
2020-05-21 11:57:11 +0800

    Update login.js

--- login/src/js/login.js
+++ login/src/js/login.js
@@ -7,11 +7,7 @@ $(document).ready(function() {
             account: $('.account_input').val(),
             password: $('.password_input').val(),
         }, (res) => {
-            const form = document.forms['form']
-            const appellation = form.elements.appellation.value;
-            const account = form.elements.account.value;
-            const password = form.elements.password.value;
-            // alert("appellation: "+appellation+"\naccount: "+account+"\npassword: "+password);
+            window.location.href = "../Home/test.html";
         });
     });
 });
