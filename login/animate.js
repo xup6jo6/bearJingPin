c2910c98bdfec44b494e32e137dd2681d66c22a1
aasd755326 <38196667+aasd755326@users.noreply.github.com>
2020-06-12 13:34:56 +0800

    Update animate.js

--- login/animate.js
+++ login/animate.js
@@ -23,10 +23,43 @@ $(document).ready(function(){
     }).then(function(){
         $('.R1').toggleClass('fadeout');
         $('.R2').toggleClass('fadeout');
+        $(".register1").fadeIn(1000);
+        $(".login1").fadeIn(1000);
+    });
+    
+    
+    $(".register1").click(function(){
         $('.logo').toggleClass('logo-1');
-        return delay(1000);
-    }).then(function(){
-        $('.typein').toggleClass('typein1');
-        return delay(0);
+        $(".register1").fadeOut(1000);
+        $(".login1").fadeOut(1000);
+        delay(1000);
+        $('.login2').toggleClass('login2S');
+        $('.register2').toggleClass('register2B disable-btn');
+        $(".login2").fadeIn(1000);
+        $(".register2").fadeIn(1000);
+        $("#registerblock").fadeIn(1000);
+    });
+    $(".login1").click(function(){
+        $('.logo').toggleClass('logo-1');
+        $(".register1").fadeOut(1000);
+        $(".login1").fadeOut(1000);
+        delay(1000); 
+        $('.login2').toggleClass('login2B disable-btn');
+        $('.register2').toggleClass('register2s');
+        $(".login2").fadeIn(1000);
+        $(".register2").fadeIn(1000);
+        $("#loginblock").fadeIn(1000);
+    });
+    $(".register2").click(function(){
+        $("#loginblock").fadeOut();
+        $('.login2').toggleClass('login2S login2B disable-btn');
+        $('.register2').toggleClass('register2S register2B disable-btn');
+        $("#registerblock").fadeIn();
+    });
+    $(".login2").click(function(){
+        $("#registerblock").fadeOut();
+        $('.login2').toggleClass('login2S login2B disable-btn');
+        $('.register2').toggleClass('register2S register2B disable-btn');
+        $("#loginblock").fadeIn();
     });
 })
